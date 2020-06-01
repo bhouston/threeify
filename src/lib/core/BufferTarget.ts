d1d7f9e756813bf36ecec5a647f7d324a1699635
Ben Houston <bhouston@threekit.com>
2020-06-01 19:29:53 -0400

    cleanup.

--- src/lib/core/BufferTarget.ts
+++ src/lib/core/BufferTarget.ts
@@ -1,10 +1,12 @@
+let GL = WebGLRenderingContext;
+
 export enum BufferTarget {
-	Array = WebGLRenderingContext.ARRAY_BUFFER, // Buffer containing vertex attributes, such as vertex coordinates, texture coordinate data, or vertex color data.
-	ElementArray = WebGLRenderingContext.ARRAY_BUFFER, // Buffer used for element indices.
-	CopyRead = WebGL2RenderingContext.COPY_READ_BUFFER, // Buffer for copying from one buffer object to another.
-	CopyWrite = WebGL2RenderingContext.COPY_WRITE_BUFFER, // Buffer for copying from one buffer object to another.
-	TransformFeedback = WebGL2RenderingContext.TRANSFORM_FEEDBACK_BUFFER, // Buffer for transform feedback operations.
-	Uniform = WebGL2RenderingContext.UNIFORM_BUFFER, // Buffer used for storing uniform blocks.
-	PixelPack = WebGL2RenderingContext.PIXEL_PACK_BUFFER, // Buffer used for pixel transfer operations.
-	PixelUnpack = WebGL2RenderingContext.PIXEL_UNPACK_BUFFER, // Buffer used for pixel transfer operations.
+	Array = GL.ARRAY_BUFFER, // Buffer containing vertex attributes, such as vertex coordinates, texture coordinate data, or vertex color data.
+	ElementArray = GL.ARRAY_BUFFER, // Buffer used for element indices.
+	CopyRead = GL.COPY_READ_BUFFER, // Buffer for copying from one buffer object to another.
+	CopyWrite = GL.COPY_WRITE_BUFFER, // Buffer for copying from one buffer object to another.
+	TransformFeedback = GL.TRANSFORM_FEEDBACK_BUFFER, // Buffer for transform feedback operations.
+	Uniform = GL.UNIFORM_BUFFER, // Buffer used for storing uniform blocks.
+	PixelPack = GL.PIXEL_PACK_BUFFER, // Buffer used for pixel transfer operations.
+	PixelUnpack = GL.PIXEL_UNPACK_BUFFER, // Buffer used for pixel transfer operations.
 }
