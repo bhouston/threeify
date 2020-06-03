5103f95313818b95754995cae4f5f76fa5efec6a
Ben Houston <bhouston@threekit.com>
2020-06-02 21:50:36 -0400

    uniforms work in progress

--- src/lib/renderers/webgl2/Uniforms/ArrayUniforms.ts
+++ src/lib/renderers/webgl2/Uniforms/ArrayUniforms.ts
@@ -1,8 +1,126 @@
 import { ComponentType } from '../../../core/ComponentType';
+import { Matrix4 } from '../../../math/Matrix4';
+import { ProgramUniform } from '../ProgramUniform';
+import { Vector2 } from '../../../math/Vector2';
+import { Vector3 } from '../../../math/Vector3';
+import { Color } from '../../../math/Color';
 
-// Array of scalars
-function setValueV1fArray(gl, v) {
-	gl.uniform1fv(this.addr, v);
+const GL2 = WebGL2RenderingContext;
+
+export enum UniformType {
+    Bool = WebGL2RenderingContext.BOOL,
+    BoolVec2 = WebGL2RenderingContext.BOOL_VEC2,
+    BoolVec3 = WebGL2RenderingContext.BOOL_VEC3,
+    BoolVec4 = WebGL2RenderingContext.BOOL_VEC4,
+
+    Int = WebGL2RenderingContext.INT,
+    IntVec2 = WebGL2RenderingContext.INT_VEC2,
+    IntVec3 = WebGL2RenderingContext.INT_VEC3,
+    IntVec4 = WebGL2RenderingContext.INT_VEC4,
+
+    Float = WebGL2RenderingContext.FLOAT,
+    FloatVec2 = WebGL2RenderingContext.FLOAT_VEC2,
+    FloatVec3 = WebGL2RenderingContext.FLOAT_VEC3,
+    FloatVec4 = WebGL2RenderingContext.FLOAT_VEC4,
+
+    FloatMat2 = WebGL2RenderingContext.FLOAT_MAT2,
+    FloatMat2x3 = WebGL2RenderingContext.FLOAT_MAT2x3,
+    FloatMat2x4 = WebGL2RenderingContext.FLOAT_MAT2x4,
+
+    FloatMat3x2 = WebGL2RenderingContext.FLOAT_MAT3x2,
+    FloatMat3 = WebGL2RenderingContext.FLOAT_MAT3,
+    FloatMat3x4 = WebGL2RenderingContext.FLOAT_MAT3x4,
+
+    FloatMat4x2 = WebGL2RenderingContext.FLOAT_MAT4x3,
+    FloatMat4x3 = WebGL2RenderingContext.FLOAT_MAT4x3,
+    FloatMat4 = WebGL2RenderingContext.FLOAT_MAT4,
+}
+
+
+export interface IUniform {
+    set( programUniform: ProgramUniform ): void;
+}
+
+// TODO: Figure out how to replace this with generics:
+// e.g. class Uniform<T> implements IUniform { value: T = new T() }.  I can not get that to compile.
+
+export class UniformFloat implements IUniform {
+    value: number;
+
+    constructor( value: number = 0.0 ) {
+        this.value = value;
+    }
+
+    set( programUniform: ProgramUniform ) {
+        let gl = programUniform.program.context.gl;
+        gl.uniform1f( programUniform.glLocation, this.value );
+    }
+}
+
+export class UniformVector2 implements IUniform {
+    value: Vector2;
+
+    constructor( value: Vector2 = new Vector2() ) {
+        this.value = value;
+    }
+
+    set( programUniform: ProgramUniform ) {
+        let gl = programUniform.program.context.gl;
+        gl.uniform2f( programUniform.glLocation, this.value.x, this.value.y );
+    }
+}
+
+export class UniformVector3 implements IUniform {
+    value: Vector3;
+
+    constructor( value: Vector3 = new Vector3() ) {
+        this.value = value;
+    }
+
+    set( programUniform: ProgramUniform ) {
+        let gl = programUniform.program.context.gl;
+        gl.uniform3f( programUniform.glLocation, this.value.x, this.value.y, this.value.z );
+    }
+}
+
+export class UniformColor implements IUniform {
+    value: Color;
+
+    constructor( value: Color = new Color() ) {
+        this.value = value;
+    }
+
+    set( programUniform: ProgramUniform ) {
+        let gl = programUniform.program.context.gl;
+        gl.uniform3f( programUniform.glLocation, this.value.r, this.value.g, this.value.b );
+    }
+}
+
+export class UniformMatrix4 implements IUniform {
+    value: Matrix4;
+
+    constructor( value: Matrix4 = new Matrix4() ) {
+        this.value = value;
+    }
+
+    set( programUniform: ProgramUniform ) {
+        let gl = programUniform.program.context.gl;
+        gl.uniformMatrix4fv( programUniform.glLocation, false, this.value.elements ); return;
+    }
+}
+
+
+export class PBRMaterialUniforms extends UniformSet {
+    albedo: UniformColor = new UniformColor( new Color(1, 1, 1));
+    roughness: UniformFloat = new UniformFloat( 0.5 );
+    metalness: UniformFloat = new UniformFloat( 0.0 );
+    emissive: UniformColor = new UniformColor( new Color(1, 1, 1));
+    normalFactor: UniformFloat = new UniformFloat( 1.0 );
+
+}
+
+export class SceneUniforms extends UniformSet {
+    worldToCameraMatrix: UniformMatrix4;
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/uniform
@@ -74,43 +192,6 @@ function setUniformMatrixValue(
 			break;
 	}	
     throw new Error('unsupported');
-}
-
-// Integer / Boolean vectors or arrays thereof (always flat arrays)
-function setValueV1iArray(gl, v) {
-	gl.uniform1iv(this.addr, v);
-}
-
-function setValueV2iArray(gl, v) {
-	gl.uniform2iv(this.addr, v);
-}
-
-function setValueV3iArray(gl, v) {
-	gl.uniform3iv(this.addr, v);
-}
-
-function setValueV4iArray(gl, v) {
-	gl.uniform4iv(this.addr, v);
-}
-
-// Array of vectors (flat or from THREE classes)
-
-function setValueV2fArray(gl, v) {
-	var data = flatten(v, this.size, 2);
-
-	gl.uniform2fv(this.addr, data);
-}
-
-function setValueV3fArray(gl, v) {
-	var data = flatten(v, this.size, 3);
-
-	gl.uniform3fv(this.addr, data);
-}
-
-function setValueV4fArray(gl, v) {
-	var data = flatten(v, this.size, 4);
-
-	gl.uniform4fv(this.addr, data);
 }
 
 // Array of matrices (flat or from THREE clases)
