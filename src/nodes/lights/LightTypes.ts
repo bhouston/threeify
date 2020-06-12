61ea1cfd68a3a4545b3df7ebb85c22505522377d
Ben Houston <bhouston@threekit.com>
2020-06-12 16:42:20 -0400

    light types enum for webgl shaders.

---
+++ src/nodes/lights/LightTypes.ts
@@ -0,0 +1,9 @@
+//
+// based on: https://github.com/KhronosGroup/glTF-Sample-Viewer/blob/master/src/shaders/punctual.glsl#L20
+//
+
+export enum LightTypes {
+  Directional = 0,
+  Point = 1,
+  Spot = 2,
+}
