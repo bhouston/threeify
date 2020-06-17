077d33ea9082682d84ad9657f131923bbacb09a2
Ben Houston <bhouston@threekit.com>
2020-06-17 08:45:53 -0400

    feat: second example, animated uniforms

--- src/examples/gettingstarted/2_animatedUniforms/index.ts
+++ src/examples/gettingstarted/2_animatedUniforms/index.ts
@@ -1,6 +1,7 @@
 import { Float32Attribute } from "../../../lib/geometry/Attribute";
 import { Geometry } from "../../../lib/geometry/Geometry";
 import { ShaderMaterial } from "../../../lib/materials/ShaderMaterial";
+import { Color } from "../../../lib/math/Color";
 import { Program, RenderingContext } from "../../../lib/renderers/webgl2";
 import { BufferGeometry } from "../../../lib/renderers/webgl2/buffers/BufferGeometry";
 import fragmentSourceCode from "./fragment.glsl";
@@ -8,7 +9,6 @@ import vertexSourceCode from "./vertex.glsl";
 
 const geometry = new Geometry();
 geometry.attributes.set("position", new Float32Attribute([0, 0.5, 0.5, -0.5, -0.5, -0.5], 2));
-geometry.attributes.set("color", new Float32Attribute([1, 0, 0, 0, 1, 0, 0, 0, 1], 3));
 
 const material = new ShaderMaterial(vertexSourceCode, fragmentSourceCode);
 
@@ -18,5 +18,15 @@ document.body.appendChild(canvasFramebuffer.canvas);
 
 const bufferGeometry = new BufferGeometry(context, geometry);
 const program = new Program(context, material);
+const uniforms = { scale: 1.0, color: new Color() };
 
-canvasFramebuffer.renderBufferGeometry(program, {}, bufferGeometry);
+canvasFramebuffer.renderBufferGeometry(program, uniforms, bufferGeometry);
+
+function animate(): void {
+  requestAnimationFrame(animate);
+  uniforms.scale = 0.6 + 0.4 * Math.cos(Date.now() * 0.001);
+  uniforms.color.setFromHSL(Date.now() * 0.001, 1.0, 0.5);
+  canvasFramebuffer.renderBufferGeometry(program, uniforms, bufferGeometry);
+}
+
+animate();
---
+++ src/examples/gettingstarted/2_animatedUniforms/vertex.glsl
@@ -0,0 +1,11 @@
+attribute vec2 position;
+
+uniform float scale;
+
+void main() {
+
+  vec2 scaledPosition = position * scale;
+
+  gl_Position = vec4( scaledPosition, 0.5, 1.0 );
+
+}
