8df88b7710afecd31ae968763da9da22bdddd611
Ben Houston <bhouston@threekit.com>
2020-06-17 13:28:33 -0400

    chore: jest should ignore /dist/

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   // Replace `ts-jest` with the preset you want to use
   // from the above list
   preset: "ts-jest",
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
