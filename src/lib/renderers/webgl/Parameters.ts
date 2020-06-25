b21edb1d1b5a99d06041692f987c636517fcfb01
Ben Houston <bhouston@threekit.com>
2020-06-24 21:50:08 -0400

    chore: minor poliish to init time

---
+++ src/lib/renderers/webgl/Parameters.ts
@@ -0,0 +1,7 @@
+export function getParameterAsString(gl: WebGLRenderingContext, parameterId: number, result = ""): string {
+  const text = gl.getParameter(parameterId);
+  if (typeof text === "string") {
+    result = text;
+  }
+  return result;
+}
