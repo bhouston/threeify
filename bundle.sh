7d56798ac2c6df7ede2299920f2684630d57c9a7
Ben Houston <bhouston@threekit.com>
2020-07-02 18:34:15 -0400

    feat: example bundler + minifier + brotli compressor script

--- bundle.sh
+++
@@ -1,3 +0,0 @@
-yarn rollup ./dist/examples/gettingstarted/11_depthTexture/index.js --file ./dist/examples/gettingstarted/11_depthTexture/index.rollup.js --format iife --minifyInternalExports
-yarn terser --compress --mangle --mangle-props -- ./dist/examples/gettingstarted/11_depthTexture/index.rollup.js --output ./dist/examples/gettingstarted/11_depthTexture/index.terser.js
-brotli ./dist/examples/gettingstarted/11_depthTexture/index.terser.js
