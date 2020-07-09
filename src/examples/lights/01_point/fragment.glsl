8eb19adc73b38c8dc6e76bd9acdbf9ad71625064
Ben Houston <neuralsoft@gmail.com>
2020-07-08 20:27:49 -0400

    feat: point light example

---
+++ src/examples/lights/01_point/fragment.glsl
@@ -0,0 +1,57 @@
+precision highp float;
+
+varying vec3 v_viewSurfacePosition;
+varying vec3 v_viewSurfaceNormal;
+varying vec2 v_uv0;
+
+uniform vec3 pointLightViewPosition;
+uniform vec3 pointLightColor;
+uniform float pointLightRange;
+
+uniform vec2      normalModulator;
+uniform sampler2D normalMap;
+
+#pragma include <brdfs/common>
+#pragma include <lighting/punctual>
+#pragma include <brdfs/ambient/basic>
+#pragma include <brdfs/diffuse/lambert>
+#pragma include <brdfs/specular/ggx>
+#pragma include <color/spaces/srgb>
+#pragma include <normals/packing>
+
+void main() {
+
+  vec3 albedo = vec3(1.,1.,1.);
+  vec3 specular = vec3(1.);
+  float specularRoughness = 0.25;
+  vec3 specularF0 = specularIntensityToF0( specular );
+
+  vec3 normal = vec3( normalModulator, 1. ) * rgbToNormal( texture2D( normalMap, v_uv0 ).rgb );
+
+  Surface surface;
+  surface.position = v_viewSurfacePosition;
+  surface.normal = normalize( v_viewSurfaceNormal );
+  surface.viewDirection = normalize( -v_viewSurfacePosition );
+
+  uvToTangentFrame( surface, v_uv0 );
+  perturbSurfaceNormal_TangentSpace( surface, normal );
+
+  PunctualLight punctualLight;
+  punctualLight.position = pointLightViewPosition;
+  punctualLight.color = pointLightColor;
+  punctualLight.range = pointLightRange;
+
+  DirectIrradiance directIrradiance;
+  pointLightToDirectIrradiance( surface, punctualLight, directIrradiance );
+
+  vec3 lightDirection = directIrradiance.lightDirection;
+  vec3 irradiance = directIrradiance.irradiance;
+
+  vec3 outputColor;
+  outputColor += irradiance * BRDF_Specular_GGX( surface, lightDirection, specularF0, specularRoughness );
+  outputColor += irradiance * BRDF_Diffuse_Lambert( albedo );
+
+  gl_FragColor.rgb = linearTosRGB( outputColor );
+  gl_FragColor.a = 1.;
+
+}
---
+++ src/examples/lights/01_point/index.ts
@@ -0,0 +1,77 @@
+import { icosahedronGeometry } from "../../../lib/geometry/primitives/polyhedronGeometry";
+import { ShaderMaterial } from "../../../lib/materials/ShaderMaterial";
+import { Euler, EulerOrder } from "../../../lib/math/Euler";
+import { Matrix4 } from "../../../lib/math/Matrix4";
+import {
+  makeMatrix4PerspectiveFov,
+  makeMatrix4RotationFromEuler,
+  makeMatrix4Translation,
+} from "../../../lib/math/Matrix4.Functions";
+import { Vector2 } from "../../../lib/math/Vector2";
+import { Vector3 } from "../../../lib/math/Vector3";
+import { makeBufferGeometryFromGeometry } from "../../../lib/renderers/webgl/buffers/BufferGeometry";
+import { ClearState } from "../../../lib/renderers/webgl/ClearState";
+import { CullingState } from "../../../lib/renderers/webgl/CullingState";
+import { DepthTestFunc, DepthTestState } from "../../../lib/renderers/webgl/DepthTestState";
+import { AttachmentBits } from "../../../lib/renderers/webgl/framebuffers/AttachmentBits";
+import { makeProgramFromShaderMaterial } from "../../../lib/renderers/webgl/programs/Program";
+import { RenderingContext } from "../../../lib/renderers/webgl/RenderingContext";
+import { makeTexImage2DFromTexture } from "../../../lib/renderers/webgl/textures/TexImage2D";
+import { fetchImage } from "../../../lib/textures/loaders/Image";
+import { Texture } from "../../../lib/textures/Texture";
+import fragmentSourceCode from "./fragment.glsl";
+import vertexSourceCode from "./vertex.glsl";
+
+async function init(): Promise<null> {
+  const geometry = icosahedronGeometry(0.75, 5);
+  const material = new ShaderMaterial(vertexSourceCode, fragmentSourceCode);
+  const normalsTexture = new Texture(await fetchImage("/assets/textures/golfball/normals2.jpg"));
+
+  const context = new RenderingContext();
+  const canvasFramebuffer = context.canvasFramebuffer;
+  if (canvasFramebuffer.canvas instanceof HTMLCanvasElement) {
+    document.body.appendChild(canvasFramebuffer.canvas);
+  }
+  const normalsMap = makeTexImage2DFromTexture(context, normalsTexture);
+  const program = makeProgramFromShaderMaterial(context, material);
+  const uniforms = {
+    // vertices
+    localToWorld: new Matrix4(),
+    worldToView: makeMatrix4Translation(new Vector3(0, 0, -3.0)),
+    viewToScreen: makeMatrix4PerspectiveFov(25, 0.1, 4.0, 1.0, canvasFramebuffer.aspectRatio),
+
+    // lights
+    pointLightViewPosition: new Vector3(0.0, 0, 0.0),
+    pointLightColor: new Vector3(1, 1, 1).multiplyByScalar(30.0),
+    pointLightRange: 6.0,
+
+    // materials
+    normalModulator: new Vector2(1, 1),
+    normalMap: normalsMap,
+  };
+  const bufferGeometry = makeBufferGeometryFromGeometry(context, geometry);
+  canvasFramebuffer.depthTestState = new DepthTestState(true, DepthTestFunc.Less);
+  canvasFramebuffer.clearState = new ClearState(new Vector3(0, 0, 0), 1.0);
+  canvasFramebuffer.cullingState = new CullingState(true);
+
+  function animate(): void {
+    const now = Date.now();
+
+    uniforms.localToWorld = makeMatrix4RotationFromEuler(
+      new Euler(0.15 * Math.PI, now * 0.0002, 0, EulerOrder.XZY),
+      uniforms.localToWorld,
+    );
+    uniforms.pointLightViewPosition = new Vector3(Math.cos(now * 0.001) * 3.0, 2.0, 0.5);
+
+    canvasFramebuffer.clear(AttachmentBits.All);
+    canvasFramebuffer.renderBufferGeometry(program, uniforms, bufferGeometry);
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+  return null;
+}
+
+init();
