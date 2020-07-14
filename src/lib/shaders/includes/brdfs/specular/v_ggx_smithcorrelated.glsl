3cb790a171beed43b9fb9a9a114bfc4657aa0934
Ben Houston <neuralsoft@gmail.com>
2020-07-14 10:08:51 -0400

    feat: be consistent in naming roughness and alphaRoughness.

--- src/lib/shaders/includes/brdfs/specular/v_ggx_smithcorrelated.glsl
+++ src/lib/shaders/includes/brdfs/specular/v_ggx_smithcorrelated.glsl
@@ -3,11 +3,10 @@
 
 // Moving Frostbite to Physically Based Rendering 3.0 - page 12, listing 2
 // https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
-float V_GGX_SmithCorrelated( const in float alpha, const in float dotNL, const in float dotNV ) {
-
-	float a2 = pow2( alpha );
+float V_GGX_SmithCorrelated( const in float alphaRoughness, const in float dotNL, const in float dotNV ) {
 
 	// dotNL and dotNV are explicitly swapped. This is not a mistake.
+	float a2 = pow2( alphaRoughness );
 	float gv = dotNL * sqrt( a2 + ( 1. - a2 ) * pow2( dotNV ) );
 	float gl = dotNV * sqrt( a2 + ( 1. - a2 ) * pow2( dotNL ) );
 
