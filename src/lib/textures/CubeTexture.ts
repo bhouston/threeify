25eac5ba3dab1f9e0292fc4566680ef513c0c153
Ben Houston <neuralsoft@gmail.com>
2020-07-16 10:54:31 -0400

    feat: cubemap transform/projection helpers

--- src/lib/textures/CubeTexture.ts
+++ src/lib/textures/CubeTexture.ts
@@ -5,17 +5,20 @@
 // * @bhouston
 //
 
+import { Matrix4 } from "../math/Matrix4";
+import { makeMatrix4LookAt } from "../math/Matrix4.Functions";
 import { Vector2 } from "../math/Vector2";
+import { Vector3 } from "../math/Vector3";
 import { IPoolUser } from "../renderers/Pool";
 import { DataType } from "../renderers/webgl/textures/DataType";
 import { PixelFormat } from "../renderers/webgl/textures/PixelFormat";
 import { TextureFilter } from "../renderers/webgl/textures/TextureFilter";
-import { ArrayBufferImage } from "./ArrayBufferImage";
-import { VirtualTexture } from "./VirtualTexture";
+import { TextureTarget } from "../renderers/webgl/textures/TextureTarget";
+import { TextureSource, VirtualTexture } from "./VirtualTexture";
 
-export class CubeTexture extends VirtualTexture implements IPoolUser {
+export class CubeMapTexture extends VirtualTexture implements IPoolUser {
   constructor(
-    public images: ArrayBufferImage[] | HTMLImageElement[],
+    public images: TextureSource[],
     level = 0,
     magFilter = TextureFilter.Linear,
     minFilter = TextureFilter.Linear,
@@ -31,3 +34,52 @@ export class CubeTexture extends VirtualTexture implements IPoolUser {
     this.size = new Vector2(images[0].width, images[0].height);
   }
 }
+
+type CubeMapFace = { target: TextureTarget; lookDirection: Vector3; upDirection: Vector3 };
+export const cubeMapFaces: Array<CubeMapFace> = [
+  {
+    target: TextureTarget.CubeMapPositiveX,
+    lookDirection: new Vector3(1, 0, 0),
+    upDirection: new Vector3(0.0, -1.0, 0.0),
+  },
+  {
+    target: TextureTarget.CubeMapNegativeX,
+    lookDirection: new Vector3(-1, 0, 0),
+    upDirection: new Vector3(0.0, -1.0, 0.0),
+  },
+  {
+    target: TextureTarget.CubeMapPositiveY,
+    lookDirection: new Vector3(0, 1, 0),
+    upDirection: new Vector3(0.0, 0.0, 1.0),
+  },
+  {
+    target: TextureTarget.CubeMapNegativeY,
+    lookDirection: new Vector3(0, -1, 0),
+    upDirection: new Vector3(0.0, 0.0, -1.0),
+  },
+  {
+    target: TextureTarget.CubeMapPositiveZ,
+    lookDirection: new Vector3(0, 0, 1),
+    upDirection: new Vector3(0.0, -1.0, 0.0),
+  },
+  {
+    target: TextureTarget.CubeMapNegativeZ,
+    lookDirection: new Vector3(0, 0, -1),
+    upDirection: new Vector3(0.0, -1.0, 0.0),
+  },
+];
+
+export function makeMatrix4CubeMapTransform(position: Vector3, faceIndex: number, result = new Matrix4()): Matrix4 {
+  return makeMatrix4LookAt(
+    position,
+    position.clone().add(cubeMapFaces[faceIndex].lookDirection),
+    cubeMapFaces[faceIndex].upDirection,
+    result,
+  );
+}
+
+/*
+export function makeMatrix4CubeMapProjection(near: number, far: number, result = new Matrix4()): Matrix4 {
+  const CUBE_PROJECTION = mat4.perspective(Math.PI / 2, aspect, near, far);
+}
+*/
