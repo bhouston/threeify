6a579896d45c11be550ee867ad101c4d1feae20e
Ben Houston <neuralsoft@gmail.com>
2020-07-19 17:27:33 -0400

    feat: add texture space to coordinate references.

--- src/lib/core/coords.ts
+++ src/lib/core/coords.ts
@@ -14,14 +14,43 @@ export class WorldSpace {
 
 // after projection matrix
 export class ClipSpace {
-  static readonly TopLeftFront = new Vector3(-1, -1, -1); // is this last coordinate correct?
-  static readonly BottomRightBack = new Vector3(1, 1, 1); // is this last coordinate correct?
+  static readonly TopLeftFront = new Vector3(-1, -1, -1);
+  static readonly BottomRightBack = new Vector3(1, 1, 1);
+
+  static readonly Top = 1;
+  static readonly Bottom = -1;
+  static readonly Left = -1;
+  static readonly Right = 1;
+
+  static readonly Near = -1;
+  static readonly Far = 1;
 }
 
 // after viewport transform
 export class ScreenSpace {
-  static readonly TopLeft = new Vector2(0, 1); // is this last coordinate correct?
-  static readonly TopRight = new Vector2(1, 1); // is this last coordinate correct?
-  static readonly BottomLeft = new Vector2(0, 0); // is this last coordinate correct?
-  static readonly BottomRight = new Vector2(1, 0); // is this last coordinate correct?
+  static readonly TopLeft = new Vector2(0, 1);
+  static readonly TopRight = new Vector2(1, 1);
+  static readonly BottomLeft = new Vector2(0, 0);
+  static readonly BottomRight = new Vector2(1, 0);
+
+  static readonly Top = 1;
+  static readonly Bottom = 0;
+  static readonly Left = 0;
+  static readonly Right = 1;
+
+  static readonly Near = 0;
+  static readonly Far = 1;
+}
+
+// this is based on how images show up in gimp and other image tools
+export class TextureSpace {
+  static readonly TopLeft = new Vector2(0, 0);
+  static readonly TopRight = new Vector2(1, 0);
+  static readonly BottomLeft = new Vector2(1, 0);
+  static readonly BottomRight = new Vector2(1, 1);
+
+  static readonly Top = 0;
+  static readonly Bottom = 1;
+  static readonly Left = 0;
+  static readonly Right = 1;
 }
