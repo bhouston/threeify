c2a4d9ce9b7c63c7925ea4a958fcc300bb175df1
Ben Houston <neuralsoft@gmail.com>
2020-07-19 07:01:52 -0400

    fix: rgbe unit tests pass.

--- src/lib/shaders/includes/color/spaces/rgbe.glsl
+++ src/lib/shaders/includes/color/spaces/rgbe.glsl
@@ -1,11 +1,11 @@
 #pragma once
 #pragma include <math/math>
 
-vec4 rgbeToLinear( in vec4 value ) {
-	return vec4( value.rgb * exp2( value.a * 255. - 128. ), 1. );
+vec3 rgbeToLinear( in vec4 value ) {
+	return vec3( value.rgb * exp2( value.a * 255. - 128. ) );
 }
 
-vec4 linearToRGBE( in vec4 value ) {
+vec4 linearToRGBE( in vec3 value ) {
 	float maxComponent = max( max( value.r, value.g ), value.b );
 	float fExp = clamp( ceil( log2( maxComponent ) ), -128., 127. );
 	return vec4( value.rgb / exp2( fExp ), ( fExp + 128. ) / 255. );
--- src/lib/shaders/includes/color/spaces/rgbe.tests.glsl
+++ src/lib/shaders/includes/color/spaces/rgbe.tests.glsl
@@ -1,20 +1,21 @@
 #pragma include <unit/fragment>
 #pragma include "rgbe"
 
-void testEquivalency( inout TestResults results, in int testId, in vec4 rgbe ) {
-  vec4 linear = rgbeToLinear( rgbe );
-  vec4 rgbe2 = linearToRGBE( linear );
-  vec4 linear2 = rgbeToLinear( rgbe2 );
-  asset( results, testId, equalsTolerance( rgbe, rgbe2, 0.0001 ) );
+void testEquivalency( inout TestResults results, in int testId, in vec3 linear ) {
+  vec4 rgbe = linearToRGBE( linear );
+  vec3 linear2 = rgbeToLinear( rgbe );
+  asset( results, testId, equalsRelativeTolerance( linear, linear2, 0.01 ) );
 }
 
 void tests( inout TestResults results ) {
 
-  testEquivalency( results, 3, vec4( 0.) );
-  testEquivalency( results, 4, vec4( 0.1) );
-  testEquivalency( results, 5, vec4( 0.2) );
-  testEquivalency( results, 6, vec4( 0.3) );
-  testEquivalency( results, 7, vec4( 0.4) );
-  testEquivalency( results, 8, vec4( 0.5) );
+  testEquivalency( results, 1, vec3( 0.0001, 0.001, 0.01 ) );
+  testEquivalency( results, 2, vec3( 0.001 ) );
+  testEquivalency( results, 3, vec3( 0.01 ) );
+  testEquivalency( results, 4, vec3( 0.1, 1.0, 10.0 ) );
+  testEquivalency( results, 5, vec3( 1.0 ) );
+  testEquivalency( results, 6, vec3( 10.0 ) );
+  testEquivalency( results, 7, vec3( 100.0 ) );
+  testEquivalency( results, 8, vec3( 1000.0, 100.0, 10.0 ) );
 
 }
