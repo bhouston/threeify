1ed06e03772ac5f25e08d31bf8f9e566b1d4bb55
Ben Houston <neuralsoft@gmail.com>
2020-07-22 11:16:38 -0400

    feat: add Vector4 tests.

--- src/lib/math/Vector2.test.ts
+++ src/lib/math/Vector2.test.ts
@@ -1,19 +1,29 @@
 import { Vector2 } from "./Vector2";
 
-test("Instancing", () => {
-  const a = new Vector2();
-  expect(a.x).toBe(0);
-  expect(a.y).toBe(0);
+describe("Vector2", () => {
+  test("constructor defaults", () => {
+    const a = new Vector2();
+    expect(a.x).toBe(0);
+    expect(a.y).toBe(0);
+  });
 
-  const b = new Vector2(1, 2);
-  expect(b.x).toBe(1);
-  expect(b.y).toBe(2);
+  test("constructor values", () => {
+    const b = new Vector2(1, 2);
+    expect(b.x).toBe(1);
+    expect(b.y).toBe(2);
+  });
 
-  const c = b.clone();
-  expect(c.x).toBe(1);
-  expect(c.y).toBe(2);
+  test("clone", () => {
+    const b = new Vector2(1, 2);
+    const c = b.clone();
+    expect(c.x).toBe(1);
+    expect(c.y).toBe(2);
+  });
 
-  const d = new Vector2().copy(b);
-  expect(d.x).toBe(1);
-  expect(d.y).toBe(2);
+  test("copy", () => {
+    const b = new Vector2(1, 2);
+    const d = new Vector2().copy(b);
+    expect(d.x).toBe(1);
+    expect(d.y).toBe(2);
+  });
 });
