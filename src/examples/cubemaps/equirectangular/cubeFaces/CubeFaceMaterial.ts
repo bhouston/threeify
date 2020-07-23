cf6a476686bd4910a687506ea758a08ef58b176e
Ben Houston <neuralsoft@gmail.com>
2020-07-23 18:07:45 -0400

    feat: makeCubeMapFromEquirectangularTexture utility function

--- src/examples/cubemaps/equirectangular/cubeFaces/CubeFaceMaterial.ts
+++
@@ -1,5 +0,0 @@
-import { ShaderMaterial } from "../../../../lib/materials/ShaderMaterial";
-import fragmentSource from "./fragment.glsl";
-import vertexSource from "./vertex.glsl";
-
-export const cubeFaceMaterial = new ShaderMaterial(vertexSource, fragmentSource);
--- src/examples/cubemaps/equirectangular/index.ts
+++ src/examples/cubemaps/equirectangular/index.ts
@@ -1,4 +1,3 @@
-import { passGeometry } from "../../../lib/geometry/primitives/passGeometry";
 import { icosahedronGeometry } from "../../../lib/geometry/primitives/polyhedronGeometry";
 import { ShaderMaterial } from "../../../lib/materials/ShaderMaterial";
 import { Euler } from "../../../lib/math/Euler";
@@ -12,62 +11,25 @@ import { Vector2 } from "../../../lib/math/Vector2";
 import { Vector3 } from "../../../lib/math/Vector3";
 import { makeBufferGeometryFromGeometry } from "../../../lib/renderers/webgl/buffers/BufferGeometry";
 import { DepthTestFunc, DepthTestState } from "../../../lib/renderers/webgl/DepthTestState";
-import { Attachment } from "../../../lib/renderers/webgl/framebuffers/Attachment";
-import { Framebuffer } from "../../../lib/renderers/webgl/framebuffers/Framebuffer";
 import { makeProgramFromShaderMaterial } from "../../../lib/renderers/webgl/programs/Program";
 import { RenderingContext } from "../../../lib/renderers/webgl/RenderingContext";
-import {
-  makeTexImage2DFromTexture,
-  makeTexImage2DFromCubeTexture,
-} from "../../../lib/renderers/webgl/textures/TexImage2D";
-import { TextureFilter } from "../../../lib/renderers/webgl/textures/TextureFilter";
-import { cubeMapFaces, CubeMapTexture } from "../../../lib/textures/CubeTexture";
+import { makeCubeMapFromEquirectangularTexture } from "../../../lib/renderers/webgl/utilities/CubeMap.Functions";
 import { fetchImage } from "../../../lib/textures/loaders/Image";
 import { Texture } from "../../../lib/textures/Texture";
 import fragmentSource from "./fragment.glsl";
 import vertexSource from "./vertex.glsl";
-import { TextureWrap } from "../../../lib/renderers/webgl/textures/TextureWrap";
-import { cubeFaceMaterial } from "./cubeFaces/CubeFaceMaterial";
 
 async function init(): Promise<null> {
-  /* const garageTexture = new Texture(await fetchImage("/assets/textures/cube/garage/equirectangular.jpg"));
-  garageTexture.wrapS = TextureWrap.Repeat;
-  garageTexture.wrapT = TextureWrap.ClampToEdge;
-  garageTexture.minFilter = TextureFilter.Linear;*/
-
   const debugTexture = new Texture(await fetchImage("/assets/textures/cube/debug/equirectangular.png"));
-  debugTexture.wrapS = TextureWrap.Repeat;
-  debugTexture.wrapT = TextureWrap.ClampToEdge;
-  debugTexture.minFilter = TextureFilter.Linear;
 
   const geometry = icosahedronGeometry(0.75, 4);
   const material = new ShaderMaterial(vertexSource, fragmentSource);
-  const imageSize = new Vector2(1024, 1024);
-  const cubeTexture = new CubeMapTexture([imageSize, imageSize, imageSize, imageSize, imageSize, imageSize]);
-  cubeTexture.generateMipmaps = true;
 
   const context = new RenderingContext(document.getElementById("framebuffer") as HTMLCanvasElement);
   const canvasFramebuffer = context.canvasFramebuffer;
   window.addEventListener("resize", () => canvasFramebuffer.resize());
 
-  const cubeFaceGeometry = passGeometry();
-  const cubeFaceProgram = makeProgramFromShaderMaterial(context, cubeFaceMaterial);
-  const cubeFaceUniforms = {
-    map: makeTexImage2DFromTexture(context, debugTexture),
-    faceIndex: 0,
-  };
-
-  const cubeFaceBufferGeometry = makeBufferGeometryFromGeometry(context, cubeFaceGeometry);
-  const cubeMap = makeTexImage2DFromCubeTexture(context, cubeTexture);
-
-  const framebuffer = new Framebuffer(context);
-  cubeMapFaces.forEach((cubeMapFace) => {
-    framebuffer.attach(Attachment.Color0, cubeMap, cubeMapFace.target, 0);
-    cubeFaceUniforms.faceIndex = cubeMapFace.index;
-
-    framebuffer.renderBufferGeometry(cubeFaceProgram, cubeFaceUniforms, cubeFaceBufferGeometry);
-  });
-  cubeMap.generateMipmaps();
+  const cubeMap = makeCubeMapFromEquirectangularTexture(context, debugTexture, new Vector2(1024, 1024));
 
   const program = makeProgramFromShaderMaterial(context, material);
   const uniforms = {
@@ -75,7 +37,7 @@ async function init(): Promise<null> {
     worldToView: makeMatrix4Translation(new Vector3(0, 0, -3.0)),
     viewToScreen: makeMatrix4PerspectiveFov(25, 0.1, 4.0, 1.0, canvasFramebuffer.aspectRatio),
     cubeMap: cubeMap,
-    mipCount: cubeTexture.mipCount,
+    mipCount: cubeMap.mipCount,
     perceptualRoughness: 0,
   };
   const bufferGeometry = makeBufferGeometryFromGeometry(context, geometry);
