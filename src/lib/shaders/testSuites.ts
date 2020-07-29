5929775184376730829f3eb16f85022c72b36f27
Ben Houston <neuralsoft@gmail.com>
2020-07-29 11:10:37 -0400

    chore: include mat2 and mat3 in glsl unit tests

--- src/lib/shaders/testSuites.ts
+++ src/lib/shaders/testSuites.ts
@@ -4,6 +4,8 @@ import rgbeTests from "./includes/color/encodings/rgbe.test.glsl";
 import srgbTests from "./includes/color/spaces/srgb.test.glsl";
 import cubeFacesTests from "./includes/cubemaps/cubeFaces.test.glsl";
 import latLongTests from "./includes/cubemaps/latLong.test.glsl";
+import mat2Tests from "./includes/math/mat2.test.glsl";
+import mat3Tests from "./includes/math/mat3.test.glsl";
 import mathTests from "./includes/math/math.test.glsl";
 import unitIntervalPackingTests from "./includes/math/unitIntervalPacking.test.glsl";
 import normalPackingTests from "./includes/normals/normalPacking.test.glsl";
@@ -41,6 +43,14 @@ export const glslTestSuites: Array<GLSLTestSuite> = [
   {
     name: "math",
     source: mathTests,
+  },
+  {
+    name: "mat2",
+    source: mat2Tests,
+  },
+  {
+    name: "mat3",
+    source: mat3Tests,
   },
   {
     name: "normalPacking",
