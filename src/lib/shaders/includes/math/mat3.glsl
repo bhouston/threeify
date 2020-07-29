4d023bc9be6be9def7ab5aaa580e6fa6e6dc7cc2
Ben Houston <neuralsoft@gmail.com>
2020-07-29 13:16:15 -0400

    feat: validated mat3 rotation on axes

--- src/lib/shaders/includes/math/mat3.glsl
+++ src/lib/shaders/includes/math/mat3.glsl
@@ -3,7 +3,24 @@
 mat3 mat3Identity() {
   return mat3( 1., 0., 0., 0., 1., 0., 0., 0., 1. );
 }
-// https://thebookofshaders.com/08/
+
+// validated against Three.js
+mat3 mat3RotateXDirection( vec2 dir ){
+  return mat3(
+    vec3( 1., 0., 0. ),
+    vec3( 0.,  dir.x, -dir.y ),
+    vec3( 0.,  dir.y,  dir.x ) );
+}
+
+// validated against Three.js
+mat3 mat3RotateYDirection( vec2 dir ){
+  return mat3(
+    vec3( dir.x, 0., dir.y ),
+    vec3( 0., 1., 0. ),
+    vec3( -dir.y, 0., dir.x ) );
+}
+
+// validated against Three.js
 mat3 mat3RotateZDirection( vec2 dir ){
   return mat3(
     vec3( dir.x, -dir.y, 0. ),
@@ -11,27 +28,12 @@ mat3 mat3RotateZDirection( vec2 dir ){
     vec3( 0., 0., 1. ) );
 }
 
-// https://thebookofshaders.com/08/
 mat3 mat3RotateX( float angle ){
-  float c = cos(angle);
-  float s = sin(angle);
-  return mat3(
-    vec3( 1., 0., 0. ),
-    vec3( 0.,  c, -s ),
-    vec3( 0.,  s,  c ) );
+  return mat3RotateXDirection( vec2( cos(angle), sin(angle) ) );
 }
-
-// https://thebookofshaders.com/08/
 mat3 mat3RotateY( float angle ){
-  float c = cos(angle);
-  float s = sin(angle);
-  return mat3(
-    vec3(  c, 0., -s ),
-    vec3( 0., 1., 0. ),
-    vec3(  s, 0.,  c ) );
+  return mat3RotateYDirection( vec2( cos(angle), sin(angle) ) );
 }
-
-// https://thebookofshaders.com/08/
 mat3 mat3RotateZ( float angle ){
   return mat3RotateZDirection( vec2( cos(angle), sin(angle) ) );
 }
