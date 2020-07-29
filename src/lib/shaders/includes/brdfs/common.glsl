e17613e06493db48db5b261da74f3264eb6f60b5
Ben Houston <neuralsoft@gmail.com>
2020-07-29 13:49:26 -0400

    chore: remove unused common.glsl code

--- src/lib/shaders/includes/brdfs/common.glsl
+++ src/lib/shaders/includes/brdfs/common.glsl
@@ -17,45 +17,6 @@ vec3 specularIntensityToF0( in vec3 specularIntensity ) {
   return specularIntensity * specularIntensity * 0.16;
 }
 
-
-/*
-mat3 makeTangentToViewTransformFromSurface( in Surface surface ) {
-  return mat3( surface.tangent, surface.bitangent, surface.normal );
-}
-
-vec3 flatSurfaceNormal( vec3 position ) {
-  return normalize( cross( dFdx( position ), dFdy( position ) ) );
-}
-
-void tangentToViewTransformFromSurfaceAndUV( inout Surface surface, in vec2 uv ) {
-  vec3 tempTangent = dFdy( uv.y ) * dFdx( surface.position ) - dFdx( uv.y ) * dFdy( surface.position );
-
-  surface.normal = normalize( surface.normal );
-  surface.tangent = normalize(tempTangent - surface.normal * dot( surface.normal, tempTangent ) );
-  surface.bitangent = -cross( surface.normal, surface.tangent );
-}
-*/
-
-
-
-/*
-mat3 rotateTangentSpace( in mat3 tangentToView, in vec2 anisotropicDirection ) {
-  // Due to anisoptry, the tangent can be further rotated around the geometric normal.
-  // NOTE: The null anisotropic direction value is (1, 0, 0)
-  return tangentToView * mat3RotateZDirection( anisotropicDirection.yx );
-}*/
-
-/*
-
-/**
- * this should be done after rotating the tangent frame
- * /
-mat3 normalMapToTangentSpaceTransform( in vec3 normal, int vec2 normalScale ) {
-  mat3 result = mat3Identity();
-  result[2] = normal;
-  return xfrm;
-}*/
-
 /**
  * this likely should be done after perturbing and rotating.
  */
