31e102839ec73f63898055e62427c2740f32a934
Ben Houston <neuralsoft@gmail.com>
2020-08-12 11:13:10 -0400

    revert: back to working code.

--- src/examples/compositing/basic/index.ts
+++ src/examples/compositing/basic/index.ts
@@ -9,38 +9,30 @@ const canvas = document.getElementById("framebuffer") as HTMLCanvasElement;
 async function init(): Promise<null> {
   const layerRenderer = new LayerRenderer(canvas);
 
-  layerRenderer.layerSize = new Vector2(2048, 2048);
+  layerRenderer.layerMaxSize = new Vector2(2048, 2048);
 
-  const splatUrl = "/assets/textures/decals/splat.png";
-  const splatImage = await fetchImage(splatUrl);
-  const splatTexImage2D = await layerRenderer.loadTexImage2D(splatUrl, splatImage);
+  const url = "/assets/textures/decals/splat.png";
+  const image = await fetchImage(url);
+  const texImage2D = await layerRenderer.loadTexImage2D(url, image);
 
-  const radialUrl = "/assets/textures/test/moireRadial.png";
-  const radialImage = await fetchImage(radialUrl);
-  const radialTexImage2D = await layerRenderer.loadTexImage2D(radialUrl, radialImage);
-
-  const concentricUrl = "/assets/textures/test/moireConcentric.png";
-  const concentricImage = await fetchImage(concentricUrl);
-  const concentricTexImage2D = await layerRenderer.loadTexImage2D(concentricUrl, concentricImage);
-
-  layerRenderer.clearState.color = new Vector3(0.2, 0.2, 0.2);
+  layerRenderer.backgroundColor = new Vector3(0.2, 0.2, 0.2);
 
   function animate(): void {
     requestAnimationFrame(animate);
 
     const layers: Layer[] = [];
-    layers.push(new Layer(layerRenderer, splatUrl, splatTexImage2D, new Vector2(750, 1500)));
-    layers.push(new Layer(layerRenderer, radialUrl, radialTexImage2D, new Vector2(1000, 0)));
-    layers.push(new Layer(layerRenderer, splatUrl, splatTexImage2D, new Vector2(500, 1000)));
-    layers.push(new Layer(layerRenderer, radialUrl, radialTexImage2D, new Vector2(1250, 500)));
-    layers.push(new Layer(layerRenderer, concentricUrl, concentricTexImage2D, new Vector2(200, 200)));
+    layers.push(new Layer(layerRenderer, url, texImage2D, new Vector2(750, 1500)));
+    layers.push(new Layer(layerRenderer, url, texImage2D, new Vector2(1000, 0)));
+    layers.push(new Layer(layerRenderer, url, texImage2D, new Vector2(500, 1000)));
+    layers.push(new Layer(layerRenderer, url, texImage2D, new Vector2(1250, 500)));
+    layers.push(new Layer(layerRenderer, url, texImage2D, new Vector2(200, 200)));
 
     // const now = Date.now();
     // layerRenderer.zoomScale = Math.sin(now * 0.0001) + 2.0;
     // layerRenderer.panPosition = new Vector2(Math.cos(now * 0.0003), Math.sin(now * 0.0002));
 
     layerRenderer.layers = layers;
-    layerRenderer.render();
+    layerRenderer.render(layerRenderer.context.canvasFramebuffer);
     // console.log("drawing splats");
   }
 
@@ -48,6 +40,7 @@ async function init(): Promise<null> {
 
   window.addEventListener("resize", () => {
     layerRenderer.context.canvasFramebuffer.resize();
+    layerRenderer.updateSize(layerRenderer.context.canvasFramebuffer);
   });
   canvas.addEventListener("mousedown", (mouseEvent: MouseEvent) => {
     if (mouseEvent.button === 0) {
@@ -62,12 +55,12 @@ async function init(): Promise<null> {
   });
   canvas.addEventListener("mousemove", (mouseEvent: MouseEvent) => {
     if (layerRenderer.zoomScale > 1) {
-      // console.log(`mouse offset ${mouseEvent.offsetX}, ${mouseEvent.offsetY}`);
+      console.log(`mouse offset ${mouseEvent.offsetX}, ${mouseEvent.offsetY}`);
       layerRenderer.panPosition = new Vector2(
         canvas.width * 0.5 - mouseEvent.offsetX,
         canvas.height * 0.5 - mouseEvent.offsetY,
       );
-      // console.log(layerRenderer.panPosition);
+      console.log(layerRenderer.panPosition);
     }
   });
 
--- src/examples/compositing/basic/layers/LayerRenderer.ts
+++ src/examples/compositing/basic/layers/LayerRenderer.ts
@@ -1,7 +1,6 @@
 import { planeGeometry } from "../../../../lib/geometry/primitives/planeGeometry";
 import { Blending } from "../../../../lib/materials/Blending";
 import { ShaderMaterial } from "../../../../lib/materials/ShaderMaterial";
-import { ceilPow2 } from "../../../../lib/math/Functions";
 import { Matrix4 } from "../../../../lib/math/Matrix4";
 import {
   makeMatrix4Concatenation,
@@ -16,13 +15,14 @@ import { transformPoint } from "../../../../lib/math/Vector3Matrix4.Functions";
 import { blendModeToBlendState, BlendState } from "../../../../lib/renderers/webgl/BlendState";
 import { BufferGeometry, makeBufferGeometryFromGeometry } from "../../../../lib/renderers/webgl/buffers/BufferGeometry";
 import { ClearState } from "../../../../lib/renderers/webgl/ClearState";
-import { Attachment } from "../../../../lib/renderers/webgl/framebuffers/Attachment";
-import { Framebuffer, makeColorAttachment } from "../../../../lib/renderers/webgl/framebuffers/Framebuffer";
-import { renderBufferGeometry } from "../../../../lib/renderers/webgl/framebuffers/VirtualFramebuffer";
+import { BufferBit } from "../../../../lib/renderers/webgl/framebuffers/BufferBit";
+import {
+  renderBufferGeometry,
+  VirtualFramebuffer,
+} from "../../../../lib/renderers/webgl/framebuffers/VirtualFramebuffer";
 import { makeProgramFromShaderMaterial, Program } from "../../../../lib/renderers/webgl/programs/Program";
 import { RenderingContext } from "../../../../lib/renderers/webgl/RenderingContext";
 import { makeTexImage2DFromTexture, TexImage2D } from "../../../../lib/renderers/webgl/textures/TexImage2D";
-import { TextureFilter } from "../../../../lib/renderers/webgl/textures/TextureFilter";
 import { TextureWrap } from "../../../../lib/renderers/webgl/textures/TextureWrap";
 import { Texture } from "../../../../lib/textures/Texture";
 import fragmentSource from "./fragment.glsl";
@@ -39,18 +39,13 @@ export class LayerRenderer {
   #blendState: BlendState;
   screenToView = new Matrix4();
   viewToScreen = new Matrix4();
-  viewportSize = new Vector2();
-  layerSize = new Vector2(0, 0);
+  renderSize = new Vector2();
+  layerMaxSize = new Vector2(1000, 1000);
   zoomScale = 1.0; // no zoom
   panPosition: Vector2 = new Vector2(0.5, 0.5); // center
   layers: Layer[] = [];
   firstRender = true;
-  clearState = new ClearState(new Vector3(1, 1, 1), 1.0);
-  framebuffer: Framebuffer | undefined;
-  framebufferSize = new Vector2(0, 0);
-  framebufferColorAttachment: TexImage2D | undefined;
-  framebufferViewToScreen = new Matrix4();
-  framebufferScreenToView = new Matrix4();
+  backgroundColor = new Vector3(1, 1, 1);
 
   constructor(canvas: HTMLCanvasElement) {
     this.context = new RenderingContext(canvas);
@@ -59,46 +54,14 @@ export class LayerRenderer {
     this.#blendState = blendModeToBlendState(Blending.Over, false);
   }
 
-  updateFramebuffer(): void {
-    // but to enable mipmaps (for filtering) we need it to be up-rounded to a power of 2 in width/height.
-    const newFramebufferSize = new Vector2();
-    newFramebufferSize.x = ceilPow2(this.layerSize.x);
-    newFramebufferSize.y = ceilPow2(this.layerSize.y);
-    if (this.framebuffer === undefined || !this.framebufferSize.equals(newFramebufferSize)) {
-      if (this.framebuffer !== undefined) {
-        this.framebuffer.dispose();
-        this.framebuffer = undefined;
-      }
-      this.framebufferSize = newFramebufferSize;
-      console.log(this.framebufferSize);
-      this.framebufferColorAttachment = makeColorAttachment(this.context, this.framebufferSize);
-      this.framebuffer = new Framebuffer(this.context);
-      this.framebuffer.attach(Attachment.Color0, this.framebufferColorAttachment);
-
-      // frame buffer is pixel aligned with layer images.
-      // framebuffer view is [ (0,0)-(framebuffer.with, framebuffer.height) ].
-      const aspectRatio = this.framebufferSize.width / this.framebufferSize.height;
-      this.framebufferViewToScreen = makeMatrix4OrthographicSimple(
-        this.framebufferSize.height,
-        new Vector2(this.framebufferSize.width * 0.5, this.framebufferSize.height * 0.5),
-        0.1,
-        3,
-        1,
-        aspectRatio,
-      );
-      this.framebufferScreenToView = makeMatrix4Inverse(this.framebufferViewToScreen);
-    }
-  }
-
   // have a sizeChanged function (for when the div changes size.)
-  updateViewport(): void {
-    const canvasFramebuffer = this.context.canvasFramebuffer;
-    const viewportSize = canvasFramebuffer.size;
-    if (!viewportSize.equals(this.viewportSize)) {
+  updateSize(framebuffer: VirtualFramebuffer): void {
+    const renderSize = framebuffer.size;
+    if (!renderSize.equals(this.renderSize)) {
       // console.log(`resizing canvas framebuffer to (${renderSize.x} ${renderSize.y})`);
-      const renderAspectRatio = viewportSize.width / viewportSize.height;
+      const renderAspectRatio = renderSize.width / renderSize.height;
       this.viewToScreen = makeMatrix4OrthographicSimple(
-        viewportSize.height,
+        renderSize.height,
         new Vector2(0.0, 0.0),
         0.1,
         3,
@@ -106,7 +69,7 @@ export class LayerRenderer {
         renderAspectRatio,
       );
       this.screenToView = makeMatrix4Inverse(this.viewToScreen);
-      this.viewportSize.copy(viewportSize);
+      this.renderSize.copy(renderSize);
     }
   }
 
@@ -121,8 +84,7 @@ export class LayerRenderer {
       const texture = new Texture(image);
       texture.wrapS = TextureWrap.ClampToEdge;
       texture.wrapT = TextureWrap.ClampToEdge;
-      texture.minFilter = TextureFilter.Linear;
-      texture.generateMipmaps = false;
+      texture.generateMipmaps = true;
       texture.anisotropicLevels = 1;
       texture.name = key;
 
@@ -135,102 +97,49 @@ export class LayerRenderer {
 
   // ask how much memory is used
   // set max size
-  // draw() - makes things fit with size of div assuming piframebufferToLayerUVScalels are square
-  render(): void {
-    // framebuffer.clear();
-    this.updateViewport();
+  // draw() - makes things fit with size of div assuming pixels are square
+  render(framebuffer: VirtualFramebuffer): void {
+    framebuffer.clear();
 
-    this.renderLayersToFramebuffer();
-
-    return;
-
-    const layerUVScale = new Vector2(
-      this.layerSize.width / this.framebufferSize.width,
-      this.layerSize.height / this.framebufferSize.height,
-    );
+    // TODO: Cache more of these matrices.
+    this.updateSize(framebuffer);
 
     // shrink to fit within render target
     const fitScale = Math.min(
-      this.viewportSize.width / this.layerSize.width,
-      this.viewportSize.height / this.layerSize.height,
+      this.renderSize.width / this.layerMaxSize.width,
+      this.renderSize.height / this.layerMaxSize.height,
     );
     const scale = fitScale * this.zoomScale;
     if (this.firstRender) {
-      console.log("viewportSize", this.viewportSize);
-      console.log("layerSize", this.layerSize);
+      console.log("renderSize", this.renderSize);
+      console.log("layerMaxSize", this.layerMaxSize);
       console.log("fitScale", fitScale);
       console.log("zoomScale", this.zoomScale);
       console.log("scale", scale);
     }
 
-    const localToWorld = makeMatrix4Scale(new Vector3(this.layerSize.width, this.layerSize.height, 1.0));
-
     // convert from layer pixel space to view space using zoom and pan
     const worldToViewTranslation = makeMatrix4Translation(
-      new Vector3(this.panPosition.x - this.layerSize.x * 0.5, this.panPosition.y - this.layerSize.y * 0.5, 0.0),
+      new Vector3(this.panPosition.x - this.layerMaxSize.x * 0.5, this.panPosition.y - this.layerMaxSize.y * 0.5, 0.0),
     );
     const worldToViewScale = makeMatrix4Scale(new Vector3(scale, scale, 1.0));
     const worldToView = makeMatrix4Concatenation(worldToViewScale, worldToViewTranslation);
 
-    const canvasFramebuffer = this.context.canvasFramebuffer;
-    // canvasFramebuffer.clear(BufferBit.All, this.clearState);
+    // TODO: cache this.  Maybe expose it as this.clearState to allow setting bg alpha.
+    const whiteClearState = new ClearState(this.backgroundColor, 1.0);
+    framebuffer.clear(BufferBit.All, whiteClearState);
 
-    const framebufferColorAttachment = this.framebufferColorAttachment;
-    if (framebufferColorAttachment === undefined) {
-      return;
-    }
-    const uniforms = {
-      screenToView: this.screenToView,
-      viewToScreen: this.viewToScreen,
-      worldToView: worldToView,
-      localToWorld: localToWorld,
-      layerMap: framebufferColorAttachment,
-      layerUVScale: layerUVScale,
-    };
-
-    if (this.firstRender) {
-      const localTopLeft = new Vector3(0, 0, 0);
-      const localBottomRight = new Vector3(1, 1, 0);
-      console.log("  local:", localTopLeft, localBottomRight);
-      const worldTopLeft = transformPoint(localTopLeft, uniforms.localToWorld);
-      const worldBottomRight = transformPoint(localBottomRight, uniforms.localToWorld);
-      console.log("  world:", worldTopLeft, worldBottomRight);
-      const viewTopLeft = transformPoint(worldTopLeft, uniforms.worldToView);
-      const viewBottomRight = transformPoint(worldBottomRight, uniforms.worldToView);
-      console.log("  view:", viewTopLeft, viewBottomRight);
-      const screenTopLeft = transformPoint(viewTopLeft, uniforms.viewToScreen);
-      const screenBottomRight = transformPoint(viewBottomRight, uniforms.viewToScreen);
-      console.log("  screen:", screenTopLeft, screenBottomRight);
-      this.firstRender = false;
-    }
-
-    // console.log(`drawing layer #${index}: ${layer.url} at ${layer.offset.x}, ${layer.offset.y}`);
-    // renderBufferGeometry(canvasFramebuffer, this.#program, uniforms, this.#bufferGeometry, undefined, this.#blendState);
-  }
-
-  private renderLayersToFramebuffer(): void {
-    this.updateFramebuffer();
-
-    const framebuffer = this.context.canvasFramebuffer;
-
-    // clear to black and full alpha.
-    framebuffer.clearState = new ClearState(new Vector3(0, 1, 0.5), 1.0);
-    framebuffer.clear();
-
-    // console.log(this.panPosition);
-    // console.log(this.zoomScale);
     this.layers.forEach((layer, index) => {
       const uniforms = {
-        screenToView: this.framebufferScreenToView,
-        viewToScreen: this.framebufferViewToScreen,
-        worldToView: new Matrix4(),
+        screenToView: this.screenToView,
+        viewToScreen: this.viewToScreen,
+        worldToView: worldToView,
         localToWorld: layer.localToWorld,
         layerMap: layer.texImage2D,
-        layerUVScale: new Vector2(1.0, 1.0),
       };
 
       if (this.firstRender) {
-        console.log(`rendering layer ${index}`);
+        console.log(`layer ${index}:`);
         const localTopLeft = new Vector3(0, 0, 0);
         const localBottomRight = new Vector3(1, 1, 0);
         console.log("  local:", localTopLeft, localBottomRight);
@@ -243,18 +152,11 @@ export class LayerRenderer {
         const screenTopLeft = transformPoint(viewTopLeft, uniforms.viewToScreen);
         const screenBottomRight = transformPoint(viewBottomRight, uniforms.viewToScreen);
         console.log("  screen:", screenTopLeft, screenBottomRight);
-        console.log(uniforms);
+        this.firstRender = false;
       }
 
       // console.log(`drawing layer #${index}: ${layer.url} at ${layer.offset.x}, ${layer.offset.y}`);
       renderBufferGeometry(framebuffer, this.#program, uniforms, this.#bufferGeometry, undefined, this.#blendState);
     });
-    this.firstRender = false;
-
-    // generate mipmaps.
-    const colorAttachment = this.framebufferColorAttachment;
-    if (colorAttachment !== undefined) {
-      colorAttachment.generateMipmaps();
-    }
   }
 }
