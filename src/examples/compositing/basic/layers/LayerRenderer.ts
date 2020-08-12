6e7e908e49a56b5abc56dac09e97710b1eeea475
Ben Houston <neuralsoft@gmail.com>
2020-08-12 12:24:36 -0400

    chore: almost working

--- src/examples/compositing/basic/layers/LayerRenderer.ts
+++ src/examples/compositing/basic/layers/LayerRenderer.ts
@@ -61,32 +61,32 @@ export class LayerRenderer {
 
   updateFramebuffer(): void {
     // but to enable mipmaps (for filtering) we need it to be up-rounded to a power of 2 in width/height.
-    const newFramebufferSize = new Vector2();
-    newFramebufferSize.x = ceilPow2(this.layerSize.x);
-    newFramebufferSize.y = ceilPow2(this.layerSize.y);
-    if (this.framebuffer === undefined || !this.framebufferSize.equals(newFramebufferSize)) {
+    const framebufferSize = new Vector2(ceilPow2(this.layerSize.x), ceilPow2(this.layerSize.y));
+    if (this.framebuffer === undefined || !this.framebufferSize.equals(framebufferSize)) {
+      console.log("updating framebuffer");
+
       if (this.framebuffer !== undefined) {
         this.framebuffer.dispose();
         this.framebuffer = undefined;
       }
-      this.framebufferSize = newFramebufferSize;
-      console.log(this.framebufferSize);
-      this.framebufferColorAttachment = makeColorAttachment(this.context, this.framebufferSize);
+      this.framebufferColorAttachment = makeColorAttachment(this.context, framebufferSize);
       this.framebuffer = new Framebuffer(this.context);
       this.framebuffer.attach(Attachment.Color0, this.framebufferColorAttachment);
 
       // frame buffer is pixel aligned with layer images.
       // framebuffer view is [ (0,0)-(framebuffer.with, framebuffer.height) ].
-      const aspectRatio = this.framebufferSize.width / this.framebufferSize.height;
+
+      const aspectRatio = framebufferSize.width / framebufferSize.height;
       this.framebufferViewToScreen = makeMatrix4OrthographicSimple(
-        this.framebufferSize.height,
-        new Vector2(this.framebufferSize.width * 0.5, this.framebufferSize.height * 0.5),
+        framebufferSize.height,
+        framebufferSize.clone().multiplyByScalar(0.5),
         0.1,
         3,
         1,
         aspectRatio,
       );
       this.framebufferScreenToView = makeMatrix4Inverse(this.framebufferViewToScreen);
+      this.framebufferSize.copy(framebufferSize);
     }
   }
 
@@ -139,14 +139,11 @@ export class LayerRenderer {
   render(): void {
     // framebuffer.clear();
     this.updateViewport();
-
     this.renderLayersToFramebuffer();
 
-    return;
-
     const layerUVScale = new Vector2(
-      this.layerSize.width / this.framebufferSize.width,
-      this.layerSize.height / this.framebufferSize.height,
+      this.layerSize.width / 2048, // this.framebufferSize.width,
+      this.layerSize.height / 2048, // this.framebufferSize.height,
     );
 
     // shrink to fit within render target
@@ -154,13 +151,14 @@ export class LayerRenderer {
       this.viewportSize.width / this.layerSize.width,
       this.viewportSize.height / this.layerSize.height,
     );
-    const scale = fitScale * this.zoomScale;
+    const layerToViewportScale = fitScale * this.zoomScale;
     if (this.firstRender) {
       console.log("viewportSize", this.viewportSize);
       console.log("layerSize", this.layerSize);
       console.log("fitScale", fitScale);
       console.log("zoomScale", this.zoomScale);
-      console.log("scale", scale);
+      console.log("layerToViewportScale", layerToViewportScale);
+      console.log("layerUVScale", layerUVScale);
     }
 
     const localToWorld = makeMatrix4Scale(new Vector3(this.layerSize.width, this.layerSize.height, 1.0));
@@ -169,11 +167,12 @@ export class LayerRenderer {
     const worldToViewTranslation = makeMatrix4Translation(
       new Vector3(this.panPosition.x - this.layerSize.x * 0.5, this.panPosition.y - this.layerSize.y * 0.5, 0.0),
     );
-    const worldToViewScale = makeMatrix4Scale(new Vector3(scale, scale, 1.0));
+    const worldToViewScale = makeMatrix4Scale(new Vector3(layerToViewportScale, layerToViewportScale, 1.0));
     const worldToView = makeMatrix4Concatenation(worldToViewScale, worldToViewTranslation);
 
     const canvasFramebuffer = this.context.canvasFramebuffer;
-    // canvasFramebuffer.clear(BufferBit.All, this.clearState);
+    canvasFramebuffer.clearState = new ClearState(new Vector3(1, 0, 0.5), 0.2);
+    canvasFramebuffer.clear();
 
     const framebufferColorAttachment = this.framebufferColorAttachment;
     if (framebufferColorAttachment === undefined) {
@@ -205,21 +204,21 @@ export class LayerRenderer {
     }
 
     // console.log(`drawing layer #${index}: ${layer.url} at ${layer.offset.x}, ${layer.offset.y}`);
-    // renderBufferGeometry(canvasFramebuffer, this.#program, uniforms, this.#bufferGeometry, undefined, this.#blendState);
+    renderBufferGeometry(canvasFramebuffer, this.#program, uniforms, this.#bufferGeometry, undefined, this.#blendState);
   }
 
-  private renderLayersToFramebuffer(): void {
+  renderLayersToFramebuffer(): void {
     this.updateFramebuffer();
-
-    const framebuffer = this.context.canvasFramebuffer;
+    const framebuffer = this.framebuffer;
+    if (framebuffer === undefined) {
+      return;
+    }
 
     // clear to black and full alpha.
     framebuffer.clearState = new ClearState(new Vector3(0, 1, 0.5), 1.0);
     framebuffer.clear();
 
-    // console.log(this.panPosition);
-    // console.log(this.zoomScale);
-    this.layers.forEach((layer, index) => {
+    this.layers.forEach((layer) => {
       const uniforms = {
         screenToView: this.framebufferScreenToView,
         viewToScreen: this.framebufferViewToScreen,
@@ -229,7 +228,7 @@ export class LayerRenderer {
         layerUVScale: new Vector2(1.0, 1.0),
       };
 
-      if (this.firstRender) {
+      /*   if (this.firstRender) {
         console.log(`rendering layer ${index}`);
         const localTopLeft = new Vector3(0, 0, 0);
         const localBottomRight = new Vector3(1, 1, 0);
@@ -244,12 +243,12 @@ export class LayerRenderer {
         const screenBottomRight = transformPoint(viewBottomRight, uniforms.viewToScreen);
         console.log("  screen:", screenTopLeft, screenBottomRight);
         console.log(uniforms);
-      }
+      }*/
 
       // console.log(`drawing layer #${index}: ${layer.url} at ${layer.offset.x}, ${layer.offset.y}`);
       renderBufferGeometry(framebuffer, this.#program, uniforms, this.#bufferGeometry, undefined, this.#blendState);
     });
-    this.firstRender = false;
+    // this.firstRender = false;
 
     // generate mipmaps.
     const colorAttachment = this.framebufferColorAttachment;
