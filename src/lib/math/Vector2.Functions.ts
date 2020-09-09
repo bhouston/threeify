cc225b4b5e918e016b7b9b1133c4616f075b108e
Ben Houston <neuralsoft@gmail.com>
2020-09-09 10:12:09 -0400

    feat: Vector2 fit and fill functions w/ tests

--- src/lib/math/Vector2.Functions.ts
+++ src/lib/math/Vector2.Functions.ts
@@ -11,3 +11,17 @@ export function makeVector2FromBaryCoordWeights(
   const v = baryCoord;
   return result.set(a.x * v.x + b.x * v.y + c.x * v.z, a.y * v.x + b.y * v.y + c.y * v.z);
 }
+
+export function makeVector2Fit(frame: Vector2, target: Vector2, result = new Vector2()): Vector2 {
+  result.copy(target);
+  const fitScale = Math.min(frame.width / result.width, frame.height / result.height);
+  result.multiplyByScalar(fitScale);
+  return result;
+}
+
+export function makeVector2Fill(frame: Vector2, target: Vector2, result = new Vector2()): Vector2 {
+  result.copy(target);
+  const fitScale = Math.max(frame.width / result.width, frame.height / result.height);
+  result.multiplyByScalar(fitScale);
+  return result;
+}
--- src/lib/math/Vector2.test.ts
+++ src/lib/math/Vector2.test.ts
@@ -1,4 +1,5 @@
 import { Vector2 } from "./Vector2";
+import { makeVector2Fill, makeVector2Fit } from "./Vector2.Functions";
 
 describe("Vector2", () => {
   test("constructor defaults", () => {
@@ -25,5 +26,89 @@ describe("Vector2", () => {
     const d = new Vector2().copy(b);
     expect(d.x).toBe(1);
     expect(d.y).toBe(2);
+  });
+});
+
+describe("makeVector2Fit", () => {
+  test("height fit", () => {
+    const target = new Vector2(20, 40);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fit(frame, target);
+
+    expect(fit.x).toBe(5);
+    expect(fit.y).toBe(10);
+  });
+
+  test("width fit", () => {
+    const target = new Vector2(40, 20);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fit(frame, target);
+
+    expect(fit.x).toBe(10);
+    expect(fit.y).toBe(5);
+  });
+
+  test("zoom fit", () => {
+    const target = new Vector2(5, 4);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fit(frame, target);
+
+    expect(fit.x).toBe(10);
+    expect(fit.y).toBe(8);
+  });
+
+  test("fit", () => {
+    const target = new Vector2(100, 100);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fit(frame, target);
+
+    expect(fit.x).toBe(10);
+    expect(fit.y).toBe(10);
+  });
+});
+
+describe("makeVector2Fill", () => {
+  test("width fill", () => {
+    const target = new Vector2(20, 40);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fill(frame, target);
+
+    expect(fit.x).toBe(10);
+    expect(fit.y).toBe(20);
+  });
+
+  test("height fill", () => {
+    const target = new Vector2(40, 20);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fill(frame, target);
+
+    expect(fit.x).toBe(20);
+    expect(fit.y).toBe(10);
+  });
+
+  test("zoom fit", () => {
+    const target = new Vector2(6, 5);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fill(frame, target);
+
+    expect(fit.x).toBe(12);
+    expect(fit.y).toBe(10);
+  });
+
+  test("fit", () => {
+    const target = new Vector2(100, 100);
+    const frame = new Vector2(10, 10);
+
+    const fit = makeVector2Fill(frame, target);
+
+    expect(fit.x).toBe(10);
+    expect(fit.y).toBe(10);
   });
 });
