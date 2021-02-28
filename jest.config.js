72feb3b848cbec55433e6bc09a28173a3e460f95
Ben Houston <neuralsoft@gmail.com>
2021-02-28 15:20:36 -0500

    cleanup: simplified development environment

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // [...]
-  // Replace `ts-jest` with the preset you want to use
-  // from the above list
-  preset: "ts-jest",
-  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-};
