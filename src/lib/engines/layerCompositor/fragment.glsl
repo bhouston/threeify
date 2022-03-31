7cfd5e0a066e5bed012ba11a3260288bef368150
arobertson0 <arobertson@threekit.com>
2022-03-31 14:29:21 -0400

    Refector mask function

--- src/lib/engines/layerCompositor/fragment.glsl
+++ src/lib/engines/layerCompositor/fragment.glsl
@@ -18,36 +18,38 @@ varying vec2 v_uv;
 #pragma include <math/math>
 #pragma include <color/spaces/srgb>
 
+float getMaskValue(){
+
+  if( maskMode == 0 ) return 1.0;
+
+  vec2 maskTexelUv = ( uvToMaskTexture * vec3( v_uv, 1.0 ) ).xy;
+  vec4 maskTexel = texture2D( maskMap, maskTexelUv, mipmapBias );
+
+  float maskValue;
+
+  if ( maskMode == 1 ) return maskTexel.a;
+  if ( maskMode == 2 ) return maskTexel.r;
+  if ( maskMode == 3 ) return 1.0 - maskTexel.a;
+  if ( maskMode == 4 ) return 1.0 - maskTexel.r;
+
+  return 1.0;
+
+}
+
 void main() {
   vec3 outputColor = vec3(0.);
   vec2 texelUv = ( uvToTexture * vec3( v_uv, 1.0 ) ).xy;
 
   vec4 layerColor = texture2D( layerMap, texelUv, mipmapBias );
 
-  if ( maskMode > 0 ) {
-    
-    vec2 maskTexelUv = ( uvToMaskTexture * vec3( v_uv, 1.0 ) ).xy;
-    vec4 maskTexel = texture2D( maskMap, maskTexelUv, mipmapBias );
+  layerColor *= getMaskValue();
 
-    float maskValue;
-
-    if( maskMode == 2 || maskMode == 4 ) {
-      maskValue = maskTexel.r;
-    } else {
-      maskValue = maskTexel.a;
-    }
-
-    if( maskMode >= 3 ) {
-      maskValue = 1.0 - maskValue;
-    }
-
-    layerColor *= maskValue;
-  }
 
   // premultiply alpha in output as the source PNG is not premultiplied
   if( convertToPremultipliedAlpha == 1 ) {
     layerColor.rgb *= layerColor.a;
   }
+  
   gl_FragColor = layerColor;
 
 }
