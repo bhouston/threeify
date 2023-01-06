1394c7257cb498e6ed829465cff7c8fb2e2d9693
Ben Houston <neuralsoft@gmail.com>
2023-01-06 11:37:02 -0500

    expand run-time shader parametrs for clear-coat.

--- src/examples/brdfs/clearcoat/fragment.glsl
+++ src/examples/brdfs/clearcoat/fragment.glsl
@@ -8,10 +8,17 @@ uniform vec3 pointLightViewPosition;
 uniform vec3 pointLightIntensity;
 uniform float pointLightRange;
 
+uniform vec3 albedoColor;
 uniform sampler2D albedoMap;
 
-uniform sampler2D clearCoatBumpMap;
+uniform float specularRoughnessFactor;
+uniform sampler2D specularRoughnessMap;
 
+uniform float clearCoatStrength;
+uniform vec3 clearCoatTint;
+uniform sampler2D clearCoatBumpMap;
+uniform float clearCoatRoughnessFactor;
+uniform sampler2D clearCoatRoughnessMap;
 
 #pragma include <lighting/punctual>
 #pragma include <brdfs/ambient/basic>
@@ -23,13 +30,15 @@ uniform sampler2D clearCoatBumpMap;
 
 void main() {
 
-  vec3 albedo = sRGBToLinear( texture2D( albedoMap, v_uv0 ).rgb );
+  vec3 albedo = sRGBToLinear( texture2D( albedoMap, v_uv0 ).rgb ) * albedoColor;
+  
   vec3 specular = vec3(0.15);
-  float specularRoughness = 0.25;
-  vec3 clearCoatF0 = vec3( 1. );
-  float clearCoatRoughness = 0.1;
   vec3 specularF0 = specularIntensityToF0( specular );
-
+  float specularRoughness = texture2D( specularRoughnessMap, v_uv0 ).r * specularRoughnessFactor;
+  
+  vec3 clearCoatF0 = vec3( clearCoatStrength ) * clearCoatTint;
+  float clearCoatRoughness = texture2D( clearCoatRoughnessMap, v_uv0 ).r * clearCoatRoughnessFactor;
+  
   vec3 position = v_viewSurfacePosition;
   vec3 normal = normalize( v_viewSurfaceNormal );
   vec3 viewDirection = normalize( -v_viewSurfacePosition );
@@ -50,6 +59,7 @@ void main() {
   float clearCoatDotNL = saturate( dot( directLight.direction, clearCoatNormal ) );
   float dotNL = saturate( dot( directLight.direction, normal ) );
 
+  // this lack energy conservation.
   vec3 outgoingRadiance;
   outgoingRadiance += directLight.radiance * clearCoatDotNL *
     BRDF_Specular_GGX( clearCoatNormal, viewDirection, directLight.direction, clearCoatF0, clearCoatRoughness ) ;
