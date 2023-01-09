6801d6e148732fe3a11880ed1cb7eac7dd0d2738
Ben Houston <neuralsoft@gmail.com>
2023-01-09 12:51:20 -0500

    first part of integrating gltf-transform.

--- src/lib/loaders/glTF/index.ts
+++ src/lib/loaders/glTF/index.ts
@@ -1,18 +1,24 @@
-// how to load a glTF?
-// fail if it isn't a glb.
-// get its json and its data block.
-// on demand loading strategy would be:
-//  load scene
-//   load nodes
-//    load meshes
-//     load materials
-//     load textures
+import { WebIO } from '@gltf-transform/core';
+import { KHRONOS_EXTENSIONS } from '@gltf-transform/extensions';
 
-export async loadGLB( url: string ): Promise{
-    geometries: Geometry[],
-    materials: Material[],
-    textures: Texture[]
-} {
+export async function loadGLTF(url: string): Promise<void> {
+  const io = new WebIO();
+  io.registerExtensions(KHRONOS_EXTENSIONS);
 
-    getGLBJsonAndBody( url)
+  const document = await io.read('input.glb');
+  const root = document.getRoot();
+  const scene = root.listScenes()[0];
+  scene.listChildren().forEach((node) => {
+    const localTransform = node.getMatrix();
+    console.log('localTransform', localTransform);
+    const mesh = node.getMesh();
+    if (mesh !== null) {
+      mesh.listPrimitives().forEach((primitive) => {
+        console.log('primitive', primitive.getName(), primitive);
+        primitive.listAttributes().forEach((attribute) => {
+          console.log('attribute', attribute.getName(), attribute);
+        });
+      });
+    }
+  });
 }
