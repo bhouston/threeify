6f714c32faddb61d7396b46230d887e5ae1d6f82
Ben Houston <neuralsoft@gmail.com>
2023-01-09 20:29:18 -0500

    more work on gltf loader.

---
+++ src/examples/glTF/loader/index.ts
@@ -0,0 +1,113 @@
+import {
+  BufferBit,
+  ClearState,
+  Color3,
+  CullingState,
+  DepthTestFunc,
+  DepthTestState,
+  Euler3,
+  EulerOrder3,
+  fetchImage,
+  icosahedronGeometry,
+  makeBufferGeometryFromGeometry,
+  makeMat4PerspectiveFov,
+  makeMat4RotationFromEuler,
+  makeMat4Translation,
+  makeProgramFromShaderMaterial,
+  makeTexImage2DFromTexture,
+  Mat4,
+  renderBufferGeometry,
+  RenderingContext,
+  ShaderMaterial,
+  Texture,
+  Vec3
+} from '../../../lib/index.js';
+import fragmentSource from './fragment.glsl';
+import vertexSource from './vertex.glsl';
+
+async function init(): Promise<null> {
+  const geometry = icosahedronGeometry(0.75, 5);
+  const material = new ShaderMaterial(vertexSource, fragmentSource);
+  const texture = new Texture(
+    await fetchImage('/assets/textures/planets/jupiter_2k.jpg')
+  );
+  const scratchesTexture = new Texture(
+    await fetchImage('/assets/textures/golfball/scratches.png')
+  );
+
+  const context = new RenderingContext(
+    document.getElementById('framebuffer') as HTMLCanvasElement
+  );
+  const { canvasFramebuffer } = context;
+  window.addEventListener('resize', () => canvasFramebuffer.resize());
+
+  const albedoMap = makeTexImage2DFromTexture(context, texture);
+  const clearCoatBumpMap = makeTexImage2DFromTexture(context, scratchesTexture);
+  const specularRoughnessMap = clearCoatBumpMap;
+  const clearCoatRoughnessMap = specularRoughnessMap;
+  const program = makeProgramFromShaderMaterial(context, material);
+  const uniforms = {
+    // vertices
+    localToWorld: new Mat4(),
+    worldToView: makeMat4Translation(new Vec3(0, 0, -3)),
+    viewToScreen: makeMat4PerspectiveFov(
+      25,
+      0.1,
+      4,
+      1,
+      canvasFramebuffer.aspectRatio
+    ),
+
+    // lights
+    pointLightViewPosition: new Vec3(0, 0, 0),
+    pointLightIntensity: new Color3(1, 1, 1).multiplyByScalar(30),
+    pointLightRange: 6,
+
+    // materials
+    albedoColor: new Color3(0.9, 0.9, 0.9),
+    albedoMap,
+
+    specularRoughnessFactor: 0.5,
+    specularRoughnessMap,
+
+    clearCoatStrength: 0.5,
+    clearCoatTint: new Color3(1, 1, 1),
+
+    clearCoatBumpMap,
+
+    clearCoatRoughnessFactor: 0.1,
+    clearCoatRoughnessMap
+  };
+  const bufferGeometry = makeBufferGeometryFromGeometry(context, geometry);
+  canvasFramebuffer.depthTestState = new DepthTestState(
+    true,
+    DepthTestFunc.Less
+  );
+  canvasFramebuffer.clearState = new ClearState(new Vec3(0, 0, 0), 1);
+  canvasFramebuffer.cullingState = new CullingState(true);
+
+  function animate(): void {
+    const now = Date.now();
+
+    uniforms.localToWorld = makeMat4RotationFromEuler(
+      new Euler3(0.15 * Math.PI, now * 0.0002, 0, EulerOrder3.XZY),
+      uniforms.localToWorld
+    );
+    uniforms.pointLightViewPosition = new Vec3(
+      Math.cos(now * 0.001) * 3,
+      2,
+      0.5
+    );
+
+    canvasFramebuffer.clear(BufferBit.All);
+    renderBufferGeometry(canvasFramebuffer, program, uniforms, bufferGeometry);
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+  return null;
+}
+
+init();
