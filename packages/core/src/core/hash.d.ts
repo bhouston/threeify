a41252f60e6906408ac8a30bbe5ba89cbd1b0710
Ben Houston <neuralsoft@gmail.com>
2023-01-19 14:25:37 -0500

    examples now work as well!

--- packages/core/src/core/hash.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function hashFloat1(v: number): number;
-export declare function hashFloat2(v0: number, v1: number): number;
-export declare function hashFloat3(v0: number, v1: number, v2: number): number;
-export declare function hashFloat4(v0: number, v1: number, v2: number, v3: number): number;
-export declare function hashFloatArray(elements: number[]): number;
-//# sourceMappingURL=hash.d.ts.map
