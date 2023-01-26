dc94065a69985292b318e8df817a9a0a1dfab4a5
Ben Houston <neuralsoft@gmail.com>
2023-01-26 14:26:50 -0500

    simplier default model

--- examples/src/nodes/glTF/index.ts
+++ examples/src/nodes/glTF/index.ts
@@ -43,7 +43,7 @@ async function init(): Promise<void> {
 
   const root = new SceneNode({ name: 'root' });
   const glTFModel = await glTFToSceneNode(
-    getGLTFUrl(GLTFModel.FlightHelmet, GLTFFormat.glTF)
+    getGLTFUrl(GLTFModel.AntiqueCamera, GLTFFormat.glTF)
   );
 
   updateNodeTree(glTFModel, sceneTreeCache);
