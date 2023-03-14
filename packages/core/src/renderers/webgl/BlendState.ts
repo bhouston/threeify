c9cac6439cb627cd8f79ddd644f4c15dfc5d5ca9
Ben Houston <neuralsoft@gmail.com>
2023-03-14 10:28:08 -0400

    more compact comments.

--- packages/core/src/renderers/webgl/BlendState.ts
+++ packages/core/src/renderers/webgl/BlendState.ts
@@ -10,82 +10,46 @@ import { Blending } from '../../materials/Blending.js';
 import { GL } from './GL.js';
 
 export enum BlendEquation {
-  /**
-   * source + destination
-   */
+  // source + destination
   Add = GL.FUNC_ADD,
-  /**
-   * source - destination
-   */
+  // source - destination
   Subtract = GL.FUNC_SUBTRACT,
-  /**
-   * destination - source
-   */
+  // destination - source
   ReverseSubtract = GL.FUNC_REVERSE_SUBTRACT
 }
 
 export enum BlendFunc {
-  /**
-   * Multiplies all colors by 0.
-   */
+  // Multiplies all colors by 0.
   Zero = GL.ZERO,
-  /**
-   * Multiplies all colors by 1.
-   */
+  // Multiplies all colors by 1.
   One = GL.ONE,
-  /**
-   * Multiplies all colors by the source colors.
-   */
+  // Multiplies all colors by the source colors.
   SourceColor = GL.SRC_COLOR,
-  /**
-   * Multiplies all colors by 1 minus each source color.
-   */
+  // Multiplies all colors by 1 minus each source color.
   OneMinusSourceColor = GL.ONE_MINUS_SRC_COLOR,
-  /**
-   * Multiplies all colors by the destination color.
-   */
+  // Multiplies all colors by the destination color.
   DestColor = GL.DST_COLOR,
-  /**
-   * Multiplies all colors by 1 minus each destination color.
-   */
+  // Multiplies all colors by 1 minus each destination color.
   OneMinusDestColor = GL.ONE_MINUS_DST_COLOR,
-  /**
-   * Multiplies all colors by the source alpha value.
-   */
+  // Multiplies all colors by the source alpha value.
   SourceAlpha = GL.SRC_ALPHA,
-  /**
-   * Multiplies all colors by 1 minus the source alpha value.
-   */
+  // Multiplies all colors by 1 minus the source alpha value.
   OneMinusSourceAlpha = GL.ONE_MINUS_SRC_ALPHA,
-  /**
-   * Multiplies all colors by the destination alpha value.
-   */
+  // Multiplies all colors by the destination alpha value.
   DestAlpha = GL.DST_ALPHA,
-  /**
-   * Multiplies all colors by 1 minus the destination alpha value.
-   */
+  // Multiplies all colors by 1 minus the destination alpha value.
   OneMinusDestAlpha = GL.ONE_MINUS_DST_ALPHA,
-  /**
-   * Multiplies all colors by a constant color.
-   */
+  // Multiplies all colors by a constant color.
   ConstantColor = GL.CONSTANT_COLOR,
-  /**
-   * Multiplies all colors by 1 minus a constant color.
-   */
+  // Multiplies all colors by 1 minus a constant color.
   OneMinusConstantColor = GL.ONE_MINUS_CONSTANT_COLOR,
-  /**
-   * Multiplies all colors by a constant alpha value.
-   */
+  // Multiplies all colors by a constant alpha value.
   ConstantAlpha = GL.CONSTANT_ALPHA,
-  /**
-   * Multiplies all colors by 1 minus a constant alpha value.
-   */
+  // Multiplies all colors by 1 minus a constant alpha value.
   OneMinusConstantAlpha = GL.ONE_MINUS_CONSTANT_ALPHA,
-  /**
-   * Multiplies the RGB colors by the smaller of either the source alpha value
-   * or the value of 1 minus the destination alpha value. The alpha value is
-   * multiplied by 1.
-   */
+  // Multiplies the RGB colors by the smaller of either the source alpha value
+  // or the value of 1 minus the destination alpha value. The alpha value is
+  // multiplied by 1.
   SourceAlphaSaturate = GL.SRC_ALPHA_SATURATE
 }
 
