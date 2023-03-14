9ec89403193096878cf0d8b5c91b7636a5fec303
Ben Houston <neuralsoft@gmail.com>
2023-03-14 15:23:22 -0400

    more attempted fixes that do not fix the issue.

--- packages/core/src/shaders/brdfs/sheen/d_charlie.glsl
+++ packages/core/src/shaders/brdfs/sheen/d_charlie.glsl
@@ -5,7 +5,7 @@ float D_Charlie(float sheenRoughness, float NdotH) {
   sheenRoughness = max(sheenRoughness, 0.000001); //clamp (0,1]
   float alphaG = sheenRoughness * sheenRoughness;
   float invR = 1.0 / alphaG;
-  float cos2h = pow2(NdotH);
+  float cos2h = safePow2(NdotH);
   float sin2h = 1.0 - cos2h;
-  return (2.0 + invR) * pow(sin2h, invR * 0.5) / (2.0 * PI);
+  return (2.0 + invR) * safePow(sin2h, invR * 0.5) / (2.0 * PI);
 }
